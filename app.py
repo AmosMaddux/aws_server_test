ad7ee5728a8b0e9411d04b83bb5f4f9a8c004d9d
Amos Maddux <amos.maddux@smoothstack.com>
2022-08-03 14:30:18 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, session, redirect, url_for, make_response
+import jinja2
+
+app = Flask(__name__)
+app.secret_key = b"batman"
+
+@app.route('/')
+def home():
+    return render_template("hello_world.html")
+
+@app.route('/name')
+def name():
+    return render_template("name.html")
+
+
+if __name__ == "__main__":
+    app.run(port=5001, debug=True)
